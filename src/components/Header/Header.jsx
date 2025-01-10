d29218d1a726c9c6539a7b4f3b9a165a5fcbe23e
Ahmed Elshiekh <114011662+RakimKai@users.noreply.github.com>
2025-01-10 20:00:00 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
     <>
       <header className="xl:text-[6rem] sm:text-[4rem] text-[3rem] flex flex-col justify-center items-center  sm:pl-14  pt-14">
         <h1 className="animate-fade-in leading-none font-extrabold">
-          Full Stack
+          Software
         </h1>
         <h2 className="z-30 animate-fade-in leading-none font-extrabold">
-          Web Developer
+          Developer
         </h2>
         <h2 className="z-30 animate-slideInThird xl:text-[3rem] sm:text-[3rem] text-[2rem] text-center font-bold text-secondary 2xl:pt-8 pt-3 opacity-0">
           with a continuous drive for learning and creating
